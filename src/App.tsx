8e254d3a9f56cb159cebdd12b3a710f58c84f750
unknown <norbitrial8@gmail.com>
2019-11-24 12:24:16 +0100

    Setup initial routing with hashrouter and created home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import { HashRouter } from 'react-router-dom';
+import { Route, Switch } from "react-router";
+import Home from './Home';
 import './App.css';
 
-const App: React.FC = () => {
-  return <>
-  </>
+const App = () => {
+  return <div className="app">
+    <h1>React Router Programmatically Redirect Examples</h1>
+    <h2>This solution represents different programmatically possible redirect options for React App. <span role="img" aria-label="redirect">➡️</span></h2>
+
+    <h3>Actions for redirect <span role="img" aria-label="action">🎬</span>:</h3>
+    <div className="actions">
+      <button>first</button>
+      <button>second</button>
+      <button>third</button>
+    </div>
+
+    <h3>Redirected route <span role="img" aria-label="route">🌌</span>:</h3>
+    <div className="routing">
+      <HashRouter>
+        <Suspense fallback={<div className="app-main-loader">loading...</div>}>
+          <Switch>
+            <Route exact path="/" children={Home} />
+          </Switch>
+        </Suspense>
+      </HashRouter>
+    </div>
+  </div>
 }
 
 export default App;
---
+++ src/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+
+const Home = () => {
+    return <>
+        <p className="title">Select actions from above to redirect programmatically to different routes.</p>
+        <p className="description">By clicking each buttons the rendered route will represent what code solution has been used to redirect.</p>
+        Let's try <span role="img" aria-label="flame">🔥</span>
+    </>
+}
+
+export default Home;
